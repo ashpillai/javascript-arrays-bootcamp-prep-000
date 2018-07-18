0253875addae88d9aab2aa9bd653bd9091a4d46d
pillai.ashwini@gmail.com <pillai.ashwini@gmail.com>
2018-07-18 09:42:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,4 +16,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array.push(element)
 }
 
-function access
+function accessElementInArray(array, index) {
+  return array[index]
+}
